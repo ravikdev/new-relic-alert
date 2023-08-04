034a54cfd9a098d1f7163f242933ae95f5535090
ravikdev <ravikumar71996@gmail.com>
2023-08-04 13:34:12 +0530

    Add files via upload

---
+++ my-awesome-nerdpack/nerdlets/react-viz/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class ReactVizNerdlet extends React.Component {
+  
+    constructor(props){
+        super(props);
+        this.state = {
+            counter : 0,
+        } 
+    }   
+
+    increment=()=>{
+        this.setState((prevState)=>({
+            counter : prevState.counter + 1,
+        }))
+    }
+    decrement = () => {
+        this.setState((prevState) => ({
+          counter: prevState.counter - 1,
+        }));
+      };
+    reset = ()=>{
+        this.setState((prevState) => ({
+            counter : 0,
+        }))
+    }
+
+  render() {
+    const {counter} = this.state;
+
+        return (
+            <div>
+                <h2>Counter : {counter} </h2>
+                <br />
+                <br />
+
+                <button className='incre button1' onClick={this.increment}>High</button>
+                
+                <button className='reset button1 ' onClick={this.reset}>Reset</button>
+                
+                <button className='decrement button1' onClick={this.decrement}>Low</button>
+                
+            </div>
+        )
+  }
+}
